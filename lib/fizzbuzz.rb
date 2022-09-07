1a9efc28893dd7b9c72b51a4c3c3a821e1137630
Sarita <saritaradia65@gmail.com>
2022-09-07 16:46:57 +0100

    Testing output

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'Fizz'
-end    
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  else 
+    number
+  end 
+end 
